32a1cc1f532bcfd1c191eb4c61c221ca0f9eaac0
Edwin Cloud <ecloud412@gmail.com>
2018-11-10 18:35:50 -0800

    :sparkles: Sidenav slide animations ooo fancy

--- scripts.js
+++ scripts.js
@@ -29,3 +29,52 @@ async function loadSubview(view) {
   });
   document.querySelector(`.sidenav > a#${view}`).classList.add('active');
 }
+
+function hideSidebar() {
+  const gridDiv = document.querySelector('.container');
+
+  // Hide text in sidebar so it doesn't look weird animating
+  document.querySelector('.sidebar').style.fontSize = "0"
+
+  // Get client width of sidebar
+  let sidebarWidth = document.querySelector('.sidebar').clientWidth;
+  const interval = sidebarWidth / 15;
+
+  // Animate last column of grid (sidebar) width down to 0 for slide right effect
+  const animation = setInterval(slideRight, 15);
+  function slideRight() {
+    if (sidebarWidth <= 0) {
+      gridDiv.style.gridTemplateColumns = `1fr 1fr 1fr 1fr 0`;
+      clearInterval(animation);
+    } else {
+      sidebarWidth -= interval;
+      gridDiv.style.gridTemplateColumns = `1fr 1fr 1fr 1fr ${sidebarWidth}px`;
+    }
+  }
+}
+
+function showSidebar() {
+
+  // Make sure we aren't already on portfolio page
+  if(document.querySelector('#portfolio').classList.contains('active')) return;
+
+  const gridDiv = document.querySelector('.container');
+
+  // Set counter and interval
+  let counter = 0;
+  const interval = 1 / 15;
+
+  // Animate last column of grid (sidebar) width up to 1fr for slide left effect
+  const animation = setInterval(slideLeft, 15);
+  function slideLeft() {
+    if (counter >= 1) {
+      gridDiv.style.gridTemplateColumns = `repeat(5, 1fr)`;
+      // Show text in sidebar
+      document.querySelector('.sidebar').style.fontSize = "1em"
+      clearInterval(animation);
+    } else {
+      counter += interval;
+      gridDiv.style.gridTemplateColumns = `1fr 1fr 1fr 1fr ${counter}fr`;
+    }
+  }
+}
